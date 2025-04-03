0651fb4a944335eb24cb1bd217a9d929b3f5cdd2
Pscodium <peter-larson@hotmail.com>
2025-04-03 14:57:23 -0300

    fix: loading time

--- src/routes/config.tsx
+++ src/routes/config.tsx
@@ -10,7 +10,7 @@ export default function Router() {
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        setTimeout(() => setLoading(false), 1000);
+        setTimeout(() => setLoading(false), 500);
     }, []);
 
     return loading ? (
